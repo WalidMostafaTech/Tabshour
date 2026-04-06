cc73c9ee3b84142d0e46d71dd1ef6b20dd3355ea
Walid Mostafa <amadalioo980@gmail.com>
2026-04-06 17:54:13 +0200

    finish all page before login

--- assets/js/loadComponent.js
+++ assets/js/loadComponent.js
@@ -7,11 +7,43 @@ const loadComponent = async (id, file) => {
 // load navbar
 loadComponent("navbar", "/components/navbar.html");
 
+// load footer
+loadComponent("footer", "/components/footer.html");
+
+// load home hero
+loadComponent("home-hero", "/components/home-hero.html");
+
+// load brand
+loadComponent("brand", "/components/brand.html");
+
 // load who we are
 loadComponent("who-we-are", "/components/who-we-are.html");
+
+// load features
+loadComponent("features", "/components/features.html");
+
+// load home-banner
+loadComponent("home-banner", "/components/home-banner.html");
 
 // load faq
 loadComponent("faq", "/components/faq.html");
 
-// load footer
-loadComponent("footer", "/components/footer.html");
+// load pricing
+loadComponent("pricing", "/components/pricing.html");
+
+// load contact
+loadComponent("contact", "/components/contact.html");
+
+// ############ about page ############
+
+// load what-we-do
+loadComponent("what-we-do", "/components/what-we-do.html");
+
+// load info-section
+loadComponent("info-section", "/components/info-section.html");
+
+// load why-tabshour
+loadComponent("why-tabshour", "/components/why-tabshour.html");
+
+// load about-banner
+loadComponent("about-banner", "/components/about-banner.html");
