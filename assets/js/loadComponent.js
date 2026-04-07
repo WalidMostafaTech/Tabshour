6552c756e92477b4a48bc02ed823882f9e284955
Walid Mostafa <amadalioo980@gmail.com>
2026-04-07 17:43:23 +0200

    remove / from links

--- assets/js/loadComponent.js
+++ assets/js/loadComponent.js
@@ -5,51 +5,48 @@ const loadComponent = async (id, file) => {
 };
 
 // load contact-icons-section
-loadComponent(
-  "contact-icons-section",
-  "/components/contact-icons-section.html",
-);
+loadComponent("contact-icons-section", "components/contact-icons-section.html");
 
 // load navbar
-loadComponent("navbar", "/components/navbar.html");
+loadComponent("navbar", "components/navbar.html");
 
 // load footer
-loadComponent("footer", "/components/footer.html");
+loadComponent("footer", "components/footer.html");
 
 // load home hero
-loadComponent("home-hero", "/components/home-hero.html");
+loadComponent("home-hero", "components/home-hero.html");
 
 // load brand
-loadComponent("brand", "/components/brand.html");
+loadComponent("brand", "components/brand.html");
 
 // load who we are
-loadComponent("who-we-are", "/components/who-we-are.html");
+loadComponent("who-we-are", "components/who-we-are.html");
 
 // load features
-loadComponent("features", "/components/features.html");
+loadComponent("features", "components/features.html");
 
 // load home-banner
-loadComponent("home-banner", "/components/home-banner.html");
+loadComponent("home-banner", "components/home-banner.html");
 
 // load faq
-loadComponent("faq", "/components/faq.html");
+loadComponent("faq", "components/faq.html");
 
 // load pricing
-loadComponent("pricing", "/components/pricing.html");
+loadComponent("pricing", "components/pricing.html");
 
 // load contact
-loadComponent("contact", "/components/contact.html");
+loadComponent("contact", "components/contact.html");
 
 // ############ about page ############
 
 // load what-we-do
-loadComponent("what-we-do", "/components/what-we-do.html");
+loadComponent("what-we-do", "components/what-we-do.html");
 
 // load info-section
-loadComponent("info-section", "/components/info-section.html");
+loadComponent("info-section", "components/info-section.html");
 
 // load why-tabshour
-loadComponent("why-tabshour", "/components/why-tabshour.html");
+loadComponent("why-tabshour", "components/why-tabshour.html");
 
 // load about-banner
-loadComponent("about-banner", "/components/about-banner.html");
+loadComponent("about-banner", "components/about-banner.html");
